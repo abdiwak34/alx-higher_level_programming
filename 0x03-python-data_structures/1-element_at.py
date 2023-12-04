67febcfc542d4a7aa4f6483034707e56005b3842
abdiwak34 <abdiwakamsalu@gmail.com>
2023-12-04 03:42:27 -0800

    correction

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,5 +2,4 @@
 def element_at(my_list, idx):
     if idx < 0 or idx >= len(my_list):
         return None
-    else:
-        print("Element at index {:d} is {}".foramt(idx, my_list[idx]))
+    return my_list[idx]
