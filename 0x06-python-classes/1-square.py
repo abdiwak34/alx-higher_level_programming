0c058bd0227d6a438694e469aa9ed94a928c5f0e
abdiwak34 <abdiwakamsalu@gmail.com>
2023-12-28 22:27:19 -0800

    correctio

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
+"""
+define the class
+"""
 
 
 class Square:
+    """
+    about the class
+    """
     def __init__(self, size):
+        """
+        intializing the square class attribute
+        """
         self.__size = size
